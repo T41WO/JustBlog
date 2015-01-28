7feb110d46a5da0cfedfbe7aa562358a8d6f363a
T41W0 <akeemtaiwo@gmail.com>
2015-01-28 16:36:29 +0000

    End of part 1

--- JustBlog/App_Start/RouteConfig.cs
+++ JustBlog/App_Start/RouteConfig.cs
@@ -14,6 +14,24 @@ namespace JustBlog
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                    "Category",
+                    "Category/{category}",
+                    new { controller = "Blog", action = "Category" }
+                    );
+
+            routes.MapRoute(
+                    "Tag",
+                    "Tag/{tag}",
+                    new { controller = "Blog", action = "Tag" }
+                    );
+
+            routes.MapRoute(
+                    "Post",
+                    "Archive/{year}/{month}/{title}",
+                    new { controller = "Blog", action = "Post" }
+                    );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Blog", action = "Posts", id = UrlParameter.Optional }
--- JustBlog/Models/ListViewModel.cs
+++ JustBlog/Models/ListViewModel.cs
@@ -9,13 +9,38 @@ namespace JustBlog.Models
 {
     public class ListViewModel
     {
-        public ListViewModel(IBlogRepository _blogRepository, int p)
+        public ListViewModel(IBlogRepository blogRepository, int p)
         {
-            Posts = _blogRepository.Posts(p - 1, 10);
-            TotalPosts = _blogRepository.TotalPosts();
+            Posts = blogRepository.Posts(p - 1, 10);
+            TotalPosts = blogRepository.TotalPosts();
+        }
+
+        public ListViewModel(IBlogRepository blogRepository, string text, string type, int p)
+        {
+            switch (type)
+            {
+                case "Tag":
+                    Posts = blogRepository.PostsForTag(text, p - 1, 10);
+                    TotalPosts = blogRepository.TotalPostsForTag(text);
+                    Tag = blogRepository.Tag(text);
+                    break;
+                case "Category":
+                    Posts = blogRepository.PostsForCategory(text, p - 1, 10);
+                    TotalPosts = blogRepository.TotalPostsForCategory(text);
+                    Category = blogRepository.Category(text);
+                    break;
+                default:
+                    Posts = blogRepository.PostsForSearch(text, p - 1, 10);
+                    TotalPosts = blogRepository.TotalPostsForSearch(text);
+                    Search = text;
+                    break;
+            }
         }
 
         public IList<Post> Posts { get; private set; }
         public int TotalPosts { get; private set; }
+        public Category Category { get; private set; }
+        public Tag Tag { get; private set; }
+        public string Search { get; private set; }
     }
 }
